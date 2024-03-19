35269adfa361715bebd29b40089184cc7a051d5e
aperkusic02 <ana.perkusic02@gmail.com>
2024-03-19 17:53:20 +0100

    asdas

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, Worl111d!");
+Console.WriteLine("Hello, Worl111dd!");
+Console.WriteLine("Novo");
